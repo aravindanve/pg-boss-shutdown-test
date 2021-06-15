c99f92fb7ff632f6cb670608d7fe61dd3b24d10f
Aravindan Ve <aravindan.ve@outlook.com>
2021-06-15 17:19:56 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# PgBoss Shutdown Test
---
+++ index.js
@@ -0,0 +1,41 @@
+const wtf = require('wtfnode');
+const Boss = require('pg-boss');
+const { DockerComposeEnvironment } = require('testcontainers');
+
+let environment;
+
+(async () => {
+  environment = await new DockerComposeEnvironment('.', 'compose.yml').up();
+
+  const boss = new Boss(
+    'postgresql://postgres:postgres@127.0.0.1:5432/postgres',
+  );
+
+  await boss.start();
+  await boss.subscribe('test event', (job) => {
+    // do nothing
+  });
+
+  // shutdown after 5 seconds
+  setTimeout(() => {
+    console.log('shutting down...');
+    (async () => {
+      await boss.stop();
+      await environment.down();
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+
+      // dump open handles
+      wtf.dump();
+
+      // // check open handles after 20 mins
+      // setTimeout(() => {
+      //   console.log('\n--- after 20 mins\n');
+
+      //   // dump open handles
+      //   wtf.dump();
+      // }, 20 * 60 * 1000);
+    })().catch(console.error);
+  }, 5 * 1000);
+
+  console.log('initialized!');
+})().catch(console.error);
