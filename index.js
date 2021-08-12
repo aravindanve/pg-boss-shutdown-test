5ec330951100568b5469f0aedf98084d805e9547
Aravindan Ve <aravindan.ve@outlook.com>
2021-08-12 19:02:15 +0530

    test 15m hang for errored jobs with pg-boss@6.1.0

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ let environment;
   );
 
   await boss.start();
-  await boss.subscribe('test event', (job) => {
-    // do nothing
+  await boss.subscribe('test event', async (job) => {
+    // throw error
+    throw new Error("I'm going to cause a timer to hang until the job lease expires in 15min");
   });
+
+  // create job
+  await boss.publish('test event');
 
   // shutdown after 5 seconds
   setTimeout(() => {
@@ -22,7 +26,7 @@ let environment;
     (async () => {
       await boss.stop();
       await environment.down();
-      await new Promise((resolve) => setTimeout(resolve, 10000));
+      await new Promise((resolve) => setTimeout(resolve, 10 * 1000));
 
       // dump open handles
       wtf.dump();
